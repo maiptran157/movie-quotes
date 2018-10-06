47d63cc7b0d22a2d088959de6698f46b7a745739
Mia Tran <maiptran157@gmail.com>
2018-10-05 18:21:23 -0700

    Complete project setup

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Welcome to Movie Quotes</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore consequatur totam expedita eum. Obcaecati natus tempora fugit nihil aliquid at, consequuntur, iure cupiditate mollitia sed animi eaque molestiae quisquam quod.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos aspernatur, unde excepturi suscipit accusamus quaerat ducimus, culpa alias nihil, temporibus quam placeat incidunt maiores sed at. Dolorem accusamus sit blanditiis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, cum? Itaque nihil unde est, magnam dolore eos minima eligendi assumenda facilis. Repellat odio, eligendi nisi nesciunt veniam dolores fugit inventore?</p>
+    </div>
+)
