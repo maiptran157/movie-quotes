3847bff89f37a2327a6611aac6c95dd1ed938dcc
Mia Tran <maiptran157@gmail.com>
2018-10-09 15:39:36 -0700

    Complete protection if not signing in

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,13 @@ export const signUp = (userInfo) => {
     return async (dispatch) => {
         try {
             const resp = await axios.post('http://api.reactprototypes.com/signup', userInfo);
-            console.log('Sign Up Response:', resp);
+            // console.log('Sign Up Response:', resp);
+            localStorage.setItem('token', resp.data.token);
+
+            dispatch({
+                type: types.SIGN_UP
+            })
+
         } catch (error) {
             console.log('Sign Up Error:', error.message)
         }
@@ -18,8 +24,21 @@ export const signUp = (userInfo) => {
 export const signIn = userInfo => async dispatch => {
     try {
         const resp = await axios.post('http://api.reactprototypes.com/signin', userInfo);
-        console.log('Sign In Response:', resp);
+        // console.log('Sign In Response:', resp);
+        localStorage.setItem('token', resp.data.token);
+
+        dispatch({
+            type: types.SIGN_IN
+        })
+
     } catch (error) {
         console.log('Sign In Error:', error.message)
     }
+}
+
+export const signOut = () => {
+    localStorage.removeItem('token');
+    return {
+        type: types.SIGN_OUT
+    };
 }
--- src/components/movie_quote.js
+++ src/components/movie_quote.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import auth from '../hoc/auth';
 
-export default props => (
-    <div className="center">
-        <h1>Movie Quote Here!</h1>
-        <h4>You had me at hello.</h4>
-    </div>
-);
+const MovieQuote = props => {
+    // props.log('From moview quotes');
+    return (
+        <div className="center">
+            <h1>Movie Quote Here!</h1>
+            <h4>You had me at hello.</h4>
+        </div>
+    )
+};
+
+export default auth(MovieQuote);
